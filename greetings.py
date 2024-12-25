134f174b2279f22fcfc706b75f375dc99a5ccd9e
FUN0K3Y <40722279+FUN0K3Y@users.noreply.github.com>
2024-12-26 02:32:02 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Ivan"
+title = "Logins"
+content = "Here I can store my logins"
+status = "Active"
+created_date = "26-12-2024"
+issue_date = "26-12-2025"
+
+print("Username:", username)
+print("Note title:", title)
+print("Note description:", content)
+print("Note status:", status)
+print("Create date:", created_date)
+print("Issue date:", issue_date)
