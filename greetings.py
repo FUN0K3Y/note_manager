315910f155c1e53ba182a35bf096db5df9c6913f
FUN0K3Y <40722279+FUN0K3Y@users.noreply.github.com>
2024-12-26 18:45:49 +0500

    26/12

--- greetings.py
+++ greetings.py
@@ -3,11 +3,11 @@ title = "Logins"
 content = "Here I can store my logins"
 status = "Active"
 created_date = "26-12-2024"
-issue_date = "26-12-2025"
+issue_date = "29-12-2025"
 
 print("Username:", username)
 print("Note title:", title)
 print("Note description:", content)
 print("Note status:", status)
-print("Create date:", created_date)
-print("Issue date:", issue_date)
+print("Created date:", created_date[0:5])
+print("Issue date:", issue_date[0:5])
