f13cbcca912effbb822701ed6dd9fa829077db65
FUN0K3Y <40722279+FUN0K3Y@users.noreply.github.com>
2024-12-27 04:42:20 +0500

    27/12/24

---
+++ add_input.py
@@ -0,0 +1,6 @@
+username = input("Ваше имя: ")
+title = input("Название заметки: ")
+content = input("Описание заметки: ")
+status = input("Статус заметки: ")
+created_date = input("Дата создания (в формате ДД-ММ-ГГ): ")
+issue_date = input("Дата завершения (в формате ДД-ММ-ГГ): ")
