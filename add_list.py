cddfd967be1976454c3aad479eebad022b676bbb
FUN0K3Y <40722279+FUN0K3Y@users.noreply.github.com>
2024-12-28 02:22:48 +0500

    28/12/24

--- add_list.py
+++ add_list.py
@@ -1,12 +1,16 @@
 username = input("Ваше имя пользователя: ")
-title = input("Название заметки: ")
 content = input("Описание заметки: ")
 status = input("Статус заметки (Активна/Завершена): ")
 created_date = input("Дата создания (в формате ДД-ММ-ГГ): ")
 issue_date = input("Дата истечения (в формате ДД-ММ-ГГ): ")
 
+first_title = input("Введите первый заголовок вашей заметки: ")
+second_title = input("Введите второй заголовок вашей заметки: ")
+third_title = input("Введите третий заголовок вашей заметки: ")
+titles = [first_title, second_title, third_title]
+
 print("Ваше имя пользователя: ", username)
-print("Заголовок:", title)
+print("Заголовки:", titles)
 print("Описание:", content)
 print("Статус:", status)
 print("Дата создания:", created_date)
